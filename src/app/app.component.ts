053382b3f3bbdc06238f8169a613ced35a56cd26
ange michaël Digbeu <digbeuange@outlook.fr>
2024-06-02 10:34:02 +0000

    After update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,6 +14,7 @@ import { AssignmentsService } from './shared/assignments.service';
 
 
 
+
 @Component({
   selector: 'app-root',
   standalone: true,
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,6 +4,7 @@ import {} from '@angular/common/http';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { HttpClientModule } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
